4e554cc3c107c5e990a464cc5997322a4be8db60
Supriya Mahato <codewithapu@gmail.com>
2023-11-08 20:52:03 +0530

    added changes

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,11 +2,6 @@ import '../styles/Globals.css'
 import type { Metadata } from 'next'
 import NavBar from '@/pages/components/NavBar'
 
-export const metadata: Metadata = {
-  title: 'Design The Future',
-  description: 'Design The Future',
-}
-
 export default function RootLayout({
   children,
 }: {
